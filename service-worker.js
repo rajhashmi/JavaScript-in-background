a71171dd22a90e7d42af7e58cfc26763cda069fd
shahid <shahidhashmiraj@gmail.com>
2024-04-14 22:10:08 +0530

    added service worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+// Service worker 
+
+// A JavaScript running in its own thread that will act as a middleware offering a local installed web server or web proxy for your PWA, including ressoureces and API calls
+
+// Runs client-side in browser's engine 
+
+// HTTP required
+
+// Installed by a web page
+
+// own Thread and lifecyle
+
+// Acts as a network proxy or local web server in the name of the real server
+
+// Abilities to run in the background
+
+// No need for user's permission
+
