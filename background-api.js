1428a089ee12252f3d5da1fd0436f46ce5ed1410
shahid <shahidhashmiraj@gmail.com>
2024-04-14 21:52:34 +0530

    Freeze event

--- background-api.js
+++ background-api.js
@@ -16,4 +16,34 @@ window.addEventListener('visibilitychange', ()=>{
         // code to execute in background when user is not using web-page
         console.log("Code to run in Background");
     }
+});
+
+
+
+// ths API is only available in chromium based browser
+
+// Freeze event 
+
+// this is is our last chance to save data.
+window.addEventListener('freeze', ()=>{
+    // Save State
+    // saveData()
+});
+
+
+//  resume
+
+window.addEventListener('resume', ()=>{
+    // we are back from Suspension
+    // No Need to restore 
+});
+
+// do we came back from suspension or it's new 
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    if(document.wasDiscarded){
+        // we are back from suspension but it was discarded so we need to restore data.
+
+        restoreData();
+    }
 })
