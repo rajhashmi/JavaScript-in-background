9bd936df044c5ab4f8581b00a2cd807bbc2ff5ff
shahid <shahidhashmiraj@gmail.com>
2024-04-14 22:41:42 +0530

    completed service worker basics

--- service-worker.js
+++ service-worker.js
@@ -16,3 +16,9 @@
 
 // No need for user's permission
 
+
+
+// service worker can be completely background and it run on it's own thread, but a part from that when we visit a web-page the service worker is activated it's do what need to be done by service worker and after that it become idle (in memory but dont do anything) after sometime when service is not doing anything then broser will ("You are not doing anything so it will stoped service worker") as I mentioned earliar that service worker can be completely background which mean you can run service worker of any site without visting is'nt a thread then if you do that and ythe service worker is perfomaing a havy task then the it can impact on system(os) you can't do that if you do then in few seconds you need to send notification to user  
+
+// visit this to know how many service worker your chrome have ==> chrome://serviceworker-internals/
+
